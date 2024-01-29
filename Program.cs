e73cee3fd5f0476d4e2236c464578e7408a7f5f1
tomthomas12 <tomytomthomas@gmail.com>
2024-01-29 15:46:13 +0000

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {   
+
+        Console.WriteLine("choose an option");
+        Console.WriteLine("+");
+        Console.WriteLine("-");
+        Console.WriteLine("*");
+        Console.WriteLine("/");
+        string option = Console.ReadLine();
+        Console.WriteLine("enter the value of x");
+        //string s =
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the value of x");
+        int y =int.Parse(Console.ReadLine());
+        if(option=="+")
+        {
+            Console.WriteLine(x+y);
+        }
+         if(option=="-")
+        {
+            Console.WriteLine(x-y);
+        }
+         if(option=="*")
+        {
+            Console.WriteLine(x*y);
+        }
+         if(option=="/")
+        {
+            Console.WriteLine(x/y);
+        }
+    }
+}
