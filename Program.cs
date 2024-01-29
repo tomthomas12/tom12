c3a359744d58148ff19de93fdd15d7b7fd8be5e6
tomthomas12 <tomytomthomas@gmail.com>
2024-01-29 15:57:34 +0000

    second commit

--- Program.cs
+++ Program.cs
@@ -2,34 +2,42 @@
 class Program
 {
     static void Main()
-    {   
-
-        Console.WriteLine("choose an option");
-        Console.WriteLine("+");
-        Console.WriteLine("-");
-        Console.WriteLine("*");
-        Console.WriteLine("/");
-        string option = Console.ReadLine();
-        Console.WriteLine("enter the value of x");
+    {   int ch = 1;
+        while(ch == 1)
+        {
+            Console.WriteLine("choose an option");
+            Console.WriteLine("+");
+            Console.WriteLine("-");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+            string option = Console.ReadLine();
+            Console.WriteLine("enter the value of x");
         //string s =
-        int x = int.Parse(Console.ReadLine());
-        Console.WriteLine("enter the value of x");
-        int y =int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value of x");
+            int y =int.Parse(Console.ReadLine());
         if(option=="+")
-        {
-            Console.WriteLine(x+y);
-        }
+            {
+                Console.WriteLine(x+y);
+            }
          if(option=="-")
-        {
-            Console.WriteLine(x-y);
-        }
+            {
+                Console.WriteLine(x-y);
+            }
          if(option=="*")
-        {
-            Console.WriteLine(x*y);
-        }
+            {
+                Console.WriteLine(x*y);
+            }
          if(option=="/")
-        {
-            Console.WriteLine(x/y);
+            {
+                Console.WriteLine(x/y);
+            }
+        Console.WriteLine("Do you want to cts(y/n)");
+        string opt = Console.ReadLine();
+        if(opt == "n")
+            {
+                ch=0;
+            }
         }
     }
 }
